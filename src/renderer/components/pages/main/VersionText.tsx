29bdc9205be474e827ad979face43143d489e1e0
undefined7887 <undefined7887@gmail.com>
2022-04-27 10:41:41 +0300

    small changes to VersionText component

--- src/renderer/components/pages/main/VersionText.tsx
+++ src/renderer/components/pages/main/VersionText.tsx
@@ -1,9 +1,10 @@
-import BaseHelper from 'renderer/helpers/BaseHelper';
 import React from 'react';
 import Styles from './VersionText.module.scss';
 
 export function VersionText() {
     return (
-        <p className={BaseHelper.classes(Styles.Mute)}>Build v{process.env.DROPLY_VERSION}</p>
+        <p className={Styles.Mute}>
+            {process.env.DROPLY_VERSION}
+        </p>
     )
 }
